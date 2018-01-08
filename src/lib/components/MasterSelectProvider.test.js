7b7dd26a840b474e7370b9d056d8447505e9526b
Luca Fabbri <luca@keul.it>
2018-01-08 18:27:05 +0100

    pages

--- src/lib/components/MasterSelectProvider.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MasterSelectProvider from './MasterSelectProvider';
-
-it('MasterSelectProvider renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MasterSelectProvider />, div);
-});
--- src/lib/index.js
+++
@@ -1,7 +0,0 @@
-// import MasterSelectProvider from './components/MasterSelectProvider';
-import MasterSelect from './components/MasterSelect';
-
-export default MasterSelect;
-// export {
-//   MasterSelectProvider
-// };
